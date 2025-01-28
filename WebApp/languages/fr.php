c2a3bfc0802a14a258096d621e46125caa14fdf1
gab696 <gf@gfphoto.ch>
2025-01-28 20:37:39 +0100

    Create fr.php
    French

---
+++ WebApp/languages/fr.php
@@ -0,0 +1,104 @@
+<?php 
+//FRENCH 
+
+$lang["page.new-transaction"] = "Nouvelle opération";
+$lang["page.transaction.added"] = "Nouvelle opération";
+$lang["page.transaction.duplicated"] = "Opération existante";
+$lang["page.transaction.updated"] = "Opération existante";
+$lang["page.show-transactions"] = "Voir les opérations";
+$lang["page.settings"] = "Réglages";
+$lang["page.settings.new"] = "Nouveaux réglages";
+$lang["page.guide"] = "Guide";
+$lang["page.about"] = "A propos";
+
+$lang["sec.username"] = "Username";
+$lang["sec.username.placeholder"] = "Insérer Username";
+$lang["sec.password"] = "Password";
+$lang["sec.password.placeholder"] = "Insérer Password";
+$lang["sec.login"] = "Login";
+$lang["sec.logout"] = "Logout";
+
+$lang["return_to_menu"] = "Retour au menu";
+
+$lang["db_successfully_updated_to_version"] = "Base de données mise à jour avec succès à la version ";
+
+$lang["trans.msg.add-next"] = "Ajouter suivant";
+$lang["trans.msg.action-added.successfully"] = "Ajoutée avec succès";
+$lang["trans.msg.action-duplicated.successfully"] = "Dupliqué avec succès";
+$lang["trans.msg.action-updated.successfully"] = "Mis à jour avec succès";
+
+$lang["trans.new.header"] = "Création d'une nouvelle opération";
+$lang["trans.new.submit"] = "Enregistrer l'opération";
+$lang["trans.update.header"] = "Modification d'une opération";
+$lang["trans.update.submit"] = "Mise à jour d'une opération";
+$lang["trans.duplicate.header"] = "Dupliquer une opération";
+$lang["trans.duplicate.submit"] = "Enregistrer une copie";
+$lang["trans.upload.label"] = "Prendre une photo ou envoyer pièce jointe";
+
+$lang["trans.date"] = "Date";
+$lang["trans.status"] = "Etat";
+$lang["trans.status.none"] = "Non pointé";
+$lang["trans.status.reconciled"] = "Pointé";
+$lang["trans.status.void"] = "Nul";
+$lang["trans.status.follow-up"] = "Suivi";
+$lang["trans.status.duplicate"] = "Duplicata";
+$lang["trans.type"] = "Type";
+$lang["trans.type.withdrawal"] = "Débit";
+$lang["trans.type.deposit"] = "Crédit";
+$lang["trans.type.transfer"] = "Transfert";
+$lang["trans.account"] = "Spécifier le compte de l'opération";
+$lang["trans.to-account"] = "Vers le compte";
+$lang["trans.payee"] = "Tiers";
+$lang["trans.payee.placeholder"] = "Préciser à qui va cette opération";
+$lang["trans.category"] = "Catégorie";
+$lang["trans.category.placeholder"] = "Choisir la catégorie";
+$lang["trans.sub-category"] = "Sous-catégorie";
+$lang["trans.sub-category.placeholder"] = "Choisir une sous-catégorie";
+$lang["trans.amount"] = "Montant";
+$lang["trans.amount.placeholder"] = "Montant";
+$lang["trans.notes"] = "Remarques";
+$lang["trans.notes.placeholder"] = "Remarque";
+
+$lang["settings.new"] = "Provide new settings to start using Money Manager EX";
+$lang["settings.default-account"] = "Default Account";
+$lang["settings.language"] = "Language";
+$lang["settings.save"] = "Save Settings";
+$lang["settings.apply"] = "Apply Settings";
+
+$lang["settings.authentication.section"] = "Authentication";
+$lang["settings.authentication.disable"] = "Disable authentication (NOT RECOMMENDED)";
+$lang["settings.authentication.username.placeholder"] = "Insert a new username";
+$lang["settings.authentication.password.update.placeholder"] = "To change insert a new password";
+$lang["settings.authentication.password.new.placeholder"] = "Insert a password";
+$lang["settings.authentication.password.confirm.placeholder"] = "Confirm new password";
+
+$lang["settings.new-trans.section"] = "New transactions";
+$lang["settings.new-trans.disable-payee.placeholder"] = "Disable payees management";
+$lang["settings.new-trans.disable-category.placeholder"] = "Disable categories management";
+
+$lang["settings.new-trans.section"] = "Desktop integration";
+
+$lang["guide.step1"] = "Copy the data below and paste them into your desktop application.";
+$lang["guide.step2"] = "Re-open desktop application to synchronize bank account(s), etc. to the WebApp from Desktop.";
+$lang["guide.step3"] = "Start using the WebApp version.";
+$lang["guide.webapp-url"] = "WebApp URL";
+$lang["guide.desktop-guid"] = "Desktop GUID";
+
+$lang["show.no_pending_trans"] = "Aucune opération en attente";
+$lang["show.current_pending_trans"] = "Opérations en attente";
+$lang["show.delete_all_selected"] = "Effacer les opérations sélectionées";
+$lang["show.add_new"] = "Ajouter nouvelle opération";
+$lang["show.delete"] = "Effacer";
+$lang["show.new"] = "Nouvelle opération";
+$lang["show.no_trans_selected"] = "Aucune opération sélectionée!";
+
+/*
+ * THESE TRANSLATIONS ARE STILL NOT USED.
+ * THEY ARE READY FOR JAVASCRIPT FILES: functions-1.2.0.js and new_transaction-1.0.4.js
+ */
+$lang["browser_not_support_html5"] = "Seems that the browser doesn't fully supports HTML5\n\nPlease make attention because it doesn't validate fields!";
+$lang["sec.password_not_matching"] = "Password doesn't match!";
+$lang["attachment.upload_failed"] = "There was an error attempting to upload the file.";
+$lang["attachment.upload_canceled"] = "The upload has been canceled by the user or the browser dropped the connection.";
+$lang["trans.msg.not_all_fields_are_valid"] = "Not all fields are valid";
+$lang["trans.msg.do_you_want_to_add_new_payee"] = "Do you want to add the new payee";
